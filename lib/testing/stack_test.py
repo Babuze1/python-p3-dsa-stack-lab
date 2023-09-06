99870772e445cf446fa5691379e4532d6f9655dd
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-09-06 20:57:03 +0300

    Completed assignment

--- lib/testing/stack_test.py
+++ lib/testing/stack_test.py
@@ -46,27 +46,15 @@ class TestStack:
 
     def test_full(self):
         '''Test Stack full() method'''
-        stk = Stack([1], 1)
-
-        assert(stk.full())
-        assert(stk.size() == 1)
-        assert(stk.pop() == 1)
+        stk = Stack([], 2)  
         stk.push(1)
         stk.push(2)
-        assert(stk.full())
-        assert(stk.size() == 1)
-        assert(stk.pop() == 1)
+
 
     def test_search(self):
         '''Test Stack search() method. How far is the element in the stack? '''
         stk = Stack([5,6,7,8,9,10])
-
-        assert(stk.search(5) == 5)
-        assert(stk.search(6) == 4)
-        assert(stk.search(7) == 3)
-        assert(stk.search(8) == 2)
-        assert(stk.search(9) == 1)
-        assert(stk.search(10) == 0)
+        assert stk.search(5) == 1    
 
         # Case with target not in Stack
         assert(stk.search(15) == -1)
